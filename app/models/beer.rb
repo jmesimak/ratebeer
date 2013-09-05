0a7ba3977a86cb655eacddb54285d34902561c15
Jerry Mesimaki <jerry.mesimaki@cs.helsinki.fi>
2013-09-05 21:09:29 +0300

    Avg rating and ratings for beer/show, exercise 4

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -4,4 +4,14 @@ class Beer < ActiveRecord::Base
   belongs_to :brewery
 
   has_many :ratings
+
+  def average_rating
+    allRatings = self.ratings
+    total = 0.0
+    allRatings.each do |rating|
+      total+=rating.score
+    end
+    return total/allRatings.size
+  end
+
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,4 +2,11 @@ class Rating < ActiveRecord::Base
   attr_accessible :beer_id, :score
 
   belongs_to :beer
+
+
+  def to_s
+    return self.beer.name + " " + self.score.to_s
+  end
+
+
 end
