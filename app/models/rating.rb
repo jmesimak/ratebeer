e169b82248d8d9d5cfe60f43027a31ae2939ce55
Jerry Mesimaki <jerry.mesimaki@cs.helsinki.fi>
2013-10-13 15:03:36 +0300

    showing five recent ratings

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -3,6 +3,8 @@ class Rating < ActiveRecord::Base
 
   belongs_to :beer
   belongs_to :user
+
+  scope :recent, Rating.limit(5).order("created_at")
   validates_presence_of :beer, :beer_id
   validates_numericality_of :score, { :greater_than_or_equal_to => 1,
                                       :less_than_or_equal_to => 50,
